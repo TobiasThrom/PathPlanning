12f4333390be5e75b58cba12c0fea3941f537526
unknown <Tobias.Throm@gmx.de>
2020-07-19 12:22:29 +0200

    initial commit

---
+++ pathPlanning.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello World!")
+    goal = False
+    collision = False
+    while(not goal and not collision):
+        # do something
+        break
+
+
+
+if __name__ == "__main__":
+    main()
