b4140e8fc9edccdc474628a46853a4474ffdaee4
unknown <Tobias.Throm@gmx.de>
2020-07-19 12:58:05 +0200

    added imports and first instance of map

--- pathPlanning.py
+++ pathPlanning.py
@@ -1,10 +1,14 @@
+import map
+import simpleAgent
+
 def main():
     print("Hello World!")
     goal = False
     collision = False
+    gridMap = map.Map(10)
     while(not goal and not collision):
-        # do something
         break
+
 
 
 
